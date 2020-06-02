edd1b98ed9fbf963cf745df4868506e0cb1482b4
Sargis <har-sargis@users.noreply.github.com>
2020-06-02 18:37:51 +0400

    fix(edit): edit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit
+          Edit edit
           {' asdfsdfdgds'}
           <code>src/App.js</code>
           {' dsfgdhfsgdhfgfghfjgfdhfjuhgfhjkuyjh'}
